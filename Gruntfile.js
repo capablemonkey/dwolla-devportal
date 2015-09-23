ac5d3ee7f78ff5830da873cc2543164eb4f9d1c6
Gordon Zheng <technix1@gmail.com>
2015-09-23 03:28:55 -0400

    use jit-grunt instead of load-grunt-tasks in order to speed up server and deploy tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,10 @@ module.exports = function (grunt) {
   // Show elapsed time after tasks run
   require('time-grunt')(grunt);
   // Load all Grunt tasks
-  require('load-grunt-tasks')(grunt);
+  require('jit-grunt')(grunt, {
+    useminPrepare: 'grunt-usemin',
+    buildcontrol: 'grunt-build-control'
+  });
 
   grunt.initConfig({
     // Configurable paths
