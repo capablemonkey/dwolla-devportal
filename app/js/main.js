8dd773c68e50e5fe1033cab1827c20a40f6edc3c
David Stancu <dstancu@nyu.edu>
2015-10-14 11:21:23 -0400

    change name of selector, add session store js stub for languages

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,7 @@
+$(document).ready(function() {
 
+    // Update value of language selector on change
+    $('#language_select').change(function () {
+      sessionStorage.setItem('current_language', $('#language_select :selected').val());
+    });
+});
