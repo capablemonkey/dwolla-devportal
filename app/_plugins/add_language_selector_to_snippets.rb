8403847e29a9bb0f06c96ce7e12fb6d5d8d16a76
David Stancu <dstancu@nyu.edu>
2015-10-15 15:34:39 -0400

    removed nokogiri dependency, changed monkeypatch to be specific to Rouge highlighter only to prevent breaking on update

--- app/_plugins/add_language_selector_to_snippets.rb
+++ app/_plugins/add_language_selector_to_snippets.rb
@@ -1,5 +1,3 @@
-require 'nokogiri' # <3
-
 module Jekyll
   module Converters
     class Markdown
@@ -7,34 +5,36 @@ module Jekyll
       # In Jekyll, RedcarpetParser is the OOB lexer configuration
       # shell. We need to monkeypatch it.
       class RedcarpetParser
-        # We are replacing the convert function with a modified version
-        def convert(content)
-          @redcarpet_extensions[:fenced_code_blocks] = !@redcarpet_extensions[:no_fenced_code_blocks]
-          @renderer.send :include, Redcarpet::Render::SmartyPants if @redcarpet_extensions[:smart]
-          markdown = Redcarpet::Markdown.new(@renderer.new(@redcarpet_extensions), @redcarpet_extensions)
 
-          # Use Redcarpet to generate HTML, load into Nokogiri
-          html = Nokogiri::HTML(markdown.render(content))
+        # We are using Rouge, so let's piggyback on the lexed data
+        # provided to WithRouge#block_code
+        module WithRouge
+          def block_code(code, lang)
+            code = "<pre>#{super}</pre>"
 
-          # Selector
-          selector = "<div class=\"code-snippet__selector\">
-            <nav>
-                <button class=\"active\">json</button>
-                <button>raw</button>
-                <button>php</button>
-                <button>ruby</button>
-                <button>python</button>
-                <button>javascript</button>
-            </nav>
-          </div>"
+            # Selector
+            selector = "<div class=\"code-snippet__selector language-#{lang}\">
+              <nav>
+                  <button class=\"active\">json</button>
+                  <button>raw</button>
+                  <button>php</button>
+                  <button>ruby</button>
+                  <button>python</button>
+                  <button>javascript</button>
+              </nav>
+            </div>"            
 
-          # Use CSS regex selector to find all code snippets
-          html.css('[class*="language-"]').each do |snippet|
-            snippet.inner_html = selector + "<div class=\"code-snippet__cnt\">#{snippet.inner_html}</div>"
+            output = selector + "<div class=\"code-snippet__cnt highlight language-#{lang}\">"
+            output << add_code_tags(code, lang)
+            output << "</div>"
           end
 
-          html.to_html
+          protected
+          def rouge_formatter(lexer)
+            Rouge::Formatters::HTML.new(:wrap => false)
+          end
         end
+
       end
     end
   end
